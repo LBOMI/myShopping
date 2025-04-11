0a65950806d6aeb22bcabe79f76f3d50c56bdd7f
LBOMI <lcil2293@naver.com>
2025-04-11 15:02:51 +0900

    providers 분리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
+// app/layout.tsx or app/rootlayout.tsx
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Navbar from '@/components/Navbar';
+import Providers from './providers'; // 방금 만든 client component
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -15,13 +17,13 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-
-
   return (
     <html lang="ko">
       <body className={`${inter.className} bg-white text-black dark:bg-zinc-900 dark:text-white transition-colors`}>
-        <Navbar/>
-        {children}
+        <Providers>
+          <Navbar />
+          {children}
+        </Providers>
       </body>
     </html>
   );
